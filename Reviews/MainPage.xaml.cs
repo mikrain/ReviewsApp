9ce12020d8d7ba8cc908d3bff05940df0dfe6502
mikrain <mikrain@gmail.com>
2015-03-27 14:56:51 -0500

    add pages

--- Reviews/MainPage.xaml.cs
+++ Reviews/MainPage.xaml.cs
@@ -29,9 +29,6 @@ namespace Reviews
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        readonly string[] _arrCounntryCodes = new[] { "AE", "AR", "AT", "AU", "BE", "BG", "BH", "CA", "CH", "CL", "CN", "CO", "CR", "CY", "CZ", "DE", "DK", "DZ", "EE", "EG", "ES", "FI", "FR", "GB", "GR", "HK", "HR", "HU", "ID", "IE", "IL", "IN", "IQ", "IT", "JO", "JP", "KW", "KZ", "LB", "LK", "LT", "LU", "LV", "LY", "MA", "MT", "MX", "MY", "NL", "NO", "NZ", "OM", "PE", "PH", "PK", "PL", "QA", "RO", "RS", "RU", "SA", "SE", "SG", "SI", "SK", "TH", "TN", "TR", "TT", "UA", "US", "UY", "VE", "VN" };
-
-
         public MainPage()
         {
             this.InitializeComponent();
@@ -56,63 +53,8 @@ namespace Reviews
         /// This parameter is typically used to configure the page.</param>
         protected async override void OnNavigatedTo(NavigationEventArgs e)
         {
-            await SearchResult();
-        }
-
-        private async Task SearchResult()
-        {
-            HttpClient client = new HttpClient();
-            var asf =
-                await
-                    client.GetStringAsync(
-                        "http://marketplaceedgeservice.windowsphone.com/v9/catalog/apps?os=8.10.14219.0&cc=US&lang=en-US&hw=520190979&dm=RM-821_apac_hong_kong_234&oemId=NOKIA&moId=&chunkSize=50&q=facebook&cf=99-1");
-            var doc = XDocument.Parse(asf);
-            XmlSerializer serializer = new XmlSerializer(typeof(SearchResult));
-            var feed = (SearchResult)serializer.Deserialize(doc.CreateReader());
-            await GetAppDetails(feed.Entry.Id);
 
         }
 
-        private async Task GetReviews(string appId, string skuId)
-        {
-            List<ReviewEntry> comments = new List<ReviewEntry>();
-            HttpClient client = new HttpClient();
-            XmlSerializer serializer = new XmlSerializer(typeof(ReviewsDetails));
-            List<Task> tasks = new List<Task>();
-
-            foreach (var arrCounntryCode in _arrCounntryCodes)
-            {
-                tasks.Add(Task.Run(async () =>
-                  {
-                      var asf = await client.GetStringAsync(
-                             string.Format("http://marketplaceedgeservice.windowsphone.com/v9/ratings/product/{0}/reviews?os={3}&cc={1}&lang=en-US&hw=520190979&dm=RM-821_apac_hong_kong_234&chunksize=10&skuId={2}&orderBy=latest", appId, arrCounntryCode, skuId, "8.10.14219.0"));
-                      if (!string.IsNullOrEmpty(asf))
-                      {
-                          var doc = XDocument.Parse(asf);
-                          var review = (ReviewsDetails)serializer.Deserialize(doc.CreateReader());
-                          lock (comments)
-                          {
-                              comments.AddRange(review.Entry);
-                          }
-                      }
-                  }));
-            }
-
-            await Task.WhenAll(tasks);
-            lstComments.ItemsSource = comments.OrderBy(entry => entry.Updated);
-        }
-
-        private async Task GetAppDetails(string appId)
-        {
-            HttpClient client = new HttpClient();
-            var asf = await client.GetStringAsync(
-                        string.Format("http://marketplaceedgeservice.windowsphone.com/v9/catalog/apps/{0}?os=8.10.14219.0&cc=US&lang=en-US&hw=520190979&dm=RM-821_apac_hong_kong_234&oemId=NOKIA&moId=&cf=99-1", appId));
-            var doc = XDocument.Parse(asf);
-            XmlSerializer serializer = new XmlSerializer(typeof(AppDetails));
-            var feed = (AppDetails)serializer.Deserialize(doc.CreateReader());
-
-
-            await GetReviews(appId, feed.Entry.SkuId);
-        }
     }
 }
--- Reviews/Models/SearchResult.cs
+++ Reviews/Models/SearchResult.cs
@@ -15,7 +15,7 @@ namespace Reviews
         public string startIndex { get; set; }
 
         [System.Xml.Serialization.XmlElementAttribute(Namespace = "http://www.w3.org/2005/Atom", ElementName = "entry")]
-        public Entry Entry { get; set; }
+        public List<Entry> Entry { get; set; }
     }
 
     [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = "http://www.w3.org/2005/Atom")]
@@ -34,16 +34,44 @@ namespace Reviews
             }
         }
 
+        public double AverageUserRatingNubmer
+        {
+            get { return Math.Round(double.Parse(AverageUserRating)/5, 1); }
+        }
+
+        public string UpdatedFormatted
+        {
+            get { return Updated.ToString("d"); }
+        }
+
         [System.Xml.Serialization.XmlElementAttribute(Namespace = "http://schemas.zune.net/catalog/apps/2008/02", ElementName = "averageUserRating")]
         public string AverageUserRating { get; set; }
 
+        [System.Xml.Serialization.XmlElementAttribute(Namespace = "http://schemas.zune.net/catalog/apps/2008/02", ElementName = "userRatingCount")]
+        public string UserRatingCount { get; set; }
+
         [System.Xml.Serialization.XmlElementAttribute(Namespace = "http://schemas.zune.net/catalog/apps/2008/02", ElementName = "version")]
         public string Version { get; set; }
+
+        [System.Xml.Serialization.XmlElementAttribute(Namespace = "http://schemas.zune.net/catalog/apps/2008/02", ElementName = "image")]
+        public ImageApp Image { get; set; }
 
         [System.Xml.Serialization.XmlElementAttribute(Namespace = "http://www.w3.org/2005/Atom", ElementName = "updated")]
         public DateTime Updated { get; set; }
 
         [System.Xml.Serialization.XmlElementAttribute(Namespace = "http://www.w3.org/2005/Atom", ElementName = "title")]
         public string Title { get; set; }
+    }
+
+    public class ImageApp
+    {
+        private string _id;
+
+        [System.Xml.Serialization.XmlElementAttribute(Namespace = "http://schemas.zune.net/catalog/apps/2008/02", ElementName = "id")]
+        public string Id
+        {
+            get { return string.Format("http://cdn.marketplaceimages.windowsphone.com/v8/images/{0}?imageType=ws_icon_small", _id.Replace("urn:uuid:", "")); }
+            set { _id = value; }
+        }
     }
 }
