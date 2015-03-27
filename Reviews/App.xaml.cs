8c018b45fe7df2a56675014c0c380ada06613b5a
mikrain <mikrain@gmail.com>
2015-03-27 13:46:52 -0500

    add reviews

--- Reviews/App.xaml.cs
+++ Reviews/App.xaml.cs
@@ -48,7 +48,7 @@ namespace Reviews
 #if DEBUG
             if (System.Diagnostics.Debugger.IsAttached)
             {
-                this.DebugSettings.EnableFrameRateCounter = true;
+                this.DebugSettings.EnableFrameRateCounter = false;
             }
 #endif
 
--- Reviews/MainPage.xaml.cs
+++ Reviews/MainPage.xaml.cs
@@ -8,6 +8,8 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 using System.Xml.Serialization;
+using Windows.ApplicationModel.Store;
+using Windows.Devices.Enumeration;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -27,6 +29,9 @@ namespace Reviews
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        readonly string[] _arrCounntryCodes = new[] { "AE", "AR", "AT", "AU", "BE", "BG", "BH", "CA", "CH", "CL", "CN", "CO", "CR", "CY", "CZ", "DE", "DK", "DZ", "EE", "EG", "ES", "FI", "FR", "GB", "GR", "HK", "HR", "HU", "ID", "IE", "IL", "IN", "IQ", "IT", "JO", "JP", "KW", "KZ", "LB", "LK", "LT", "LU", "LV", "LY", "MA", "MT", "MX", "MY", "NL", "NO", "NZ", "OM", "PE", "PH", "PK", "PL", "QA", "RO", "RS", "RU", "SA", "SE", "SG", "SI", "SK", "TH", "TN", "TR", "TT", "UA", "US", "UY", "VE", "VN" };
+
+
         public MainPage()
         {
             this.InitializeComponent();
@@ -37,10 +42,10 @@ namespace Reviews
             //icon http://cdn.marketplaceimages.windowsphone.com/v8/images/2f9c349d-e43e-42ae-a890-137154dddf3a?imageType=ws_icon_large
             //another details https://services.apps.microsoft.com/browse/6.2.9200-1/615/en-US_en-US/c/US/cp/10005001/Apps/72a6ba17-2d4e-4a1c-bcfb-cdc5d4b32d0e
             //details app http://marketplaceedgeservice.windowsphone.com/v9/catalog/apps/5653a21d-3739-4d12-ab82-b389b06ffae9?os=8.10.14219.0&cc=US&lang=en-US&hw=520190979&dm=RM-821_apac_hong_kong_234&oemId=NOKIA&moId=&cf=99-1
-            //reviews https://services.apps.microsoft.com/4R/6.2.9200-1/1/en-US/m/US/Apps/f514d64b-8705-43b7-a400-c4f4f3dedfc0/Reviews/all/s/date/1/pn/1
+            //reviews http://marketplaceedgeservice.windowsphone.com/v9/ratings/product/ab1b9a80-a8bd-41d0-b8b4-771d3b0ceaee/reviews?os=8.10.14219.0&cc=US&lang=en-US&hw=520190979&dm=RM-821_apac_hong_kong_234&chunksize=10&skuId=5ec7c261-b5ba-4912-a579-46adb4c3d9b7&orderBy=mostHelpful
+            //http://marketplaceedgeservice.windowsphone.com/v9/ratings/product/ab1b9a80-a8bd-41d0-b8b4-771d3b0ceaee/reviews?os=8.10.14219.0&cc=US&lang=en-US&hw=520190979&dm=RM-821_apac_hong_kong_234&chunksize=10&skuId=5ec7c261-b5ba-4912-a579-46adb4c3d9b7&orderBy=latest
             //queries http://cdn.marketplaceedgeservice.windowsphone.com/v8/catalog/queries?os=8.10.14219.0&zLocale=EN-CA&cc=US&lang=en-US&prefix=filmit&chunksize=4&includeApplications=true&includeAlbums=fal
             //search http://marketplaceedgeservice.windowsphone.com/v9/catalog/apps?os=8.10.14219.0&cc=US&lang=en-US&hw=520190979&dm=RM-821_apac_hong_kong_234&oemId=NOKIA&moId=&chunkSize=50&q=filmit&cf=99-1
-            var arrCounntryCodes = new[] { "AE", "AR", "AT", "AU", "BE", "BG", "BH", "CA", "CH", "CL", "CN", "CO", "CR", "CY", "CZ", "DE", "DK", "DZ", "EE", "EG", "ES", "FI", "FR", "GB", "GR", "HK", "HR", "HU", "ID", "IE", "IL", "IN", "IQ", "IT", "JO", "JP", "KW", "KZ", "LB", "LK", "LT", "LU", "LV", "LY", "MA", "MT", "MX", "MY", "NL", "NO", "NZ", "OM", "PE", "PH", "PK", "PL", "QA", "RO", "RS", "RU", "SA", "SE", "SG", "SI", "SK", "TH", "TN", "TR", "TT", "UA", "US", "UY", "VE", "VN" };
 
         }
 
@@ -60,21 +65,54 @@ namespace Reviews
             var asf =
                 await
                     client.GetStringAsync(
-                        "http://marketplaceedgeservice.windowsphone.com/v9/catalog/apps?os=8.10.14219.0&cc=US&lang=en-US&hw=520190979&dm=RM-821_apac_hong_kong_234&oemId=NOKIA&moId=&chunkSize=50&q=filmit&cf=99-1");
+                        "http://marketplaceedgeservice.windowsphone.com/v9/catalog/apps?os=8.10.14219.0&cc=US&lang=en-US&hw=520190979&dm=RM-821_apac_hong_kong_234&oemId=NOKIA&moId=&chunkSize=50&q=facebook&cf=99-1");
             var doc = XDocument.Parse(asf);
             XmlSerializer serializer = new XmlSerializer(typeof(SearchResult));
             var feed = (SearchResult)serializer.Deserialize(doc.CreateReader());
             await GetAppDetails(feed.Entry.Id);
+
+        }
+
+        private async Task GetReviews(string appId, string skuId)
+        {
+            List<ReviewEntry> comments = new List<ReviewEntry>();
+            HttpClient client = new HttpClient();
+            XmlSerializer serializer = new XmlSerializer(typeof(ReviewsDetails));
+            List<Task> tasks = new List<Task>();
+
+            foreach (var arrCounntryCode in _arrCounntryCodes)
+            {
+                tasks.Add(Task.Run(async () =>
+                  {
+                      var asf = await client.GetStringAsync(
+                             string.Format("http://marketplaceedgeservice.windowsphone.com/v9/ratings/product/{0}/reviews?os={3}&cc={1}&lang=en-US&hw=520190979&dm=RM-821_apac_hong_kong_234&chunksize=10&skuId={2}&orderBy=latest", appId, arrCounntryCode, skuId, "8.10.14219.0"));
+                      if (!string.IsNullOrEmpty(asf))
+                      {
+                          var doc = XDocument.Parse(asf);
+                          var review = (ReviewsDetails)serializer.Deserialize(doc.CreateReader());
+                          lock (comments)
+                          {
+                              comments.AddRange(review.Entry);
+                          }
+                      }
+                  }));
+            }
+
+            await Task.WhenAll(tasks);
+            lstComments.ItemsSource = comments.OrderBy(entry => entry.Updated);
         }
 
         private async Task GetAppDetails(string appId)
         {
             HttpClient client = new HttpClient();
             var asf = await client.GetStringAsync(
-                        string.Format("http://marketplaceedgeservice.windowsphone.com/v9/catalog/apps/{0}?os=8.10.14219.0&cc=US&lang=en-US&hw=520190979&dm=RM-821_apac_hong_kong_234&oemId=NOKIA&moId=&cf=99-1",appId));
+                        string.Format("http://marketplaceedgeservice.windowsphone.com/v9/catalog/apps/{0}?os=8.10.14219.0&cc=US&lang=en-US&hw=520190979&dm=RM-821_apac_hong_kong_234&oemId=NOKIA&moId=&cf=99-1", appId));
             var doc = XDocument.Parse(asf);
-            //XmlSerializer serializer = new XmlSerializer(typeof(SearchResult));
-            //var feed = (SearchResult)serializer.Deserialize(doc.CreateReader());
+            XmlSerializer serializer = new XmlSerializer(typeof(AppDetails));
+            var feed = (AppDetails)serializer.Deserialize(doc.CreateReader());
+
+
+            await GetReviews(appId, feed.Entry.SkuId);
         }
     }
 }
